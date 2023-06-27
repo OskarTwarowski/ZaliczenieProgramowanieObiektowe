5bc7731c47f4a4018dd75052badaaaf001ba3f2d
OskarTwarowski <134192444+OskarTwarowski@users.noreply.github.com>
2023-06-27 10:59:30 +0200

    Add files via upload

---
+++ 1/Osoba.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace   Zad_1
+{
+    internal class Osoba
+    {
+        public Osoba(string imięNazwisko)
+        {
+            ImięNazwisko = imięNazwisko;
+        }
+
+        private string? imię;
+        public string? nazwisko;
+        public DateTime? DataUrodzenia = null;
+        public DateTime? DataŚmierci = null;
+
+
+        public string? Imię
+        {
+            get => imię;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(Imię));
+                }
+                else
+                {
+                    imię = value;
+                }
+            }
+        }
+
+
+        //string ImięNazwisko - przy odczycie skleja imię i nazwisko z użyciem spacji; przy zapisie rozbija napis względem spacji i traktuje pierwszą składową jako Imię,
+        //a ostatnią jako Nazwisko; w wypadku jednoczłonowego value ustawia puste Nazwisko;
+
+        public string ImięNazwisko
+        {
+            get => $"{Imię} {nazwisko}";
+            set
+            {
+                string[] rozbicie = value.Split(' ');
+                Imię = rozbicie[0];
+                nazwisko = (rozbicie.Length > 1) ? rozbicie[^1] : "";
+            }
+        }
+
+
+        //TimeSpan? Wiek - tylko do odczytu
+        public TimeSpan? Wiek 
+        {
+            get
+            {
+                if (DataUrodzenia == null)
+                {
+                    return null;
+                }
+                if (DataŚmierci == null)
+                {
+                    return DateTime.Today - DataUrodzenia.Value;
+                }
+                else
+                {
+                    return DataŚmierci.Value - DataUrodzenia.Value;
+                }
+            }
+        }
+    }
+}
---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Zad_1;
+
+Osoba osoba = new("Oskar Twarowski")
+{
+    DataUrodzenia = new DateTime(2001, 8, 16),
+    DataŚmierci = new DateTime(2023, 6, 10)
+};
+Console.WriteLine("Zadanie 1 \n");
+Console.WriteLine($"imię: {osoba.Imię}, ");
+Console.WriteLine($"nazwisko: {osoba.nazwisko}, ");
+Console.WriteLine($"Imię i nazwisko: {osoba.ImięNazwisko}, ");
+Console.WriteLine($"Data urodzenia: {osoba.DataUrodzenia}, ");
+Console.WriteLine($"Data śmierci: {osoba.DataŚmierci},  ");
+Console.WriteLine($"wiek: {osoba.Wiek.Value.Days / 365} lat ");
+Console.WriteLine($"wiek w dniach: {osoba.Wiek.Value.Days} dni \n\n");
+
+Console.WriteLine("Wyjątki właściwości Imię:");
+osoba.Imię = "null";  //jeśli zostawimy samo null wyrzuci nam błąd ArgumentNullException ponieważ value == null 
+Console.WriteLine(osoba.Imię);
